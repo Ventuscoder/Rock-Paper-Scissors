a7938886d2c4b4a7a9915ea43d601af872f0c2f5
ventuscoder <shivankcode@gmail.com>
2020-10-19 12:52:43 +0530

    Updated rps.js with comments and code revision

--- rps.js
+++ rps.js
@@ -1,10 +1,16 @@
+// initialize basic variables
 const opts = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
+
+// This function chooses a random element from the array of moves
 function computerPlay(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
+
+// This 'helper' function executes a switch statement and gives the result of the two moves
 function getResult(playerSelection, computerSelection) {
+    // This switch basically checks and compares the moves
     switch (true) {
         case (playerSelection == "rock") && (computerSelection == "paper") :
             computerScore += 1;
@@ -16,7 +22,7 @@ function getResult(playerSelection, computerSelection) {
             break;
         case (playerSelection == "paper") && (computerSelection == "scissors") :
             computerScore += 1;
-            return "You lose! Scissors beats Paper";
+            return "You lose! Scissors beats Paper!";
             break;
         case (playerSelection == "paper") && (computerSelection == "rock") :
             playerScore += 1;
@@ -28,7 +34,7 @@ function getResult(playerSelection, computerSelection) {
             break;
         case (playerSelection == "scissors") && (computerSelection == "paper") :
             playerScore += 1;
-            return "You win! Scissors!";
+            return "You win! Scissors beats Paper!";
             break;
         default:
             playerscore += 1;
@@ -37,16 +43,22 @@ function getResult(playerSelection, computerSelection) {
             break;            
     }
 }
+
+// This function plays one single round
 function playRound() {
+    // This asks for user input in the browser window and converts the text to lowercase
     let playerMove = prompt("Please enter your move:", "e.g. rock, paper, or scissors").toLowerCase();
     let computerMove = computerPlay(opts);
     let result = getResult(playerMove, computerMove);
     console.log(result);
 }
+
+// This is the main game function
 function game() {
     for (let i = 1; i <= 5; i++) {
         playRound();
     }
+    // This checks who has won, since a draw is not possible with 5 rounds
     if (computerScore > playerScore) {
         console.log(`That's it! The computer wins by a score of ${computerScore} - ${playerScore}`);
     } else {
