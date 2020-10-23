5941831a72e4cb10b219609bed33862c384334f5
ventuscoder <shivankcode@gmail.com>
2020-10-23 07:53:54 +0530

    Added more script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const moves = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
-
+let gameOn = true;
 // This function plays a move for the computer
 function computerPlay(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -14,49 +14,71 @@ function getResult(playerSelection, computerSelection) {
     switch (true) {
         case (playerSelection == "rock") && (computerSelection == "paper") :
             computerScore += 1;
-            return "You lose! Paper beats rock!";
+            return ["You lose! Paper beats rock!", 'red'];
             break;
         case (playerSelection == "rock") && (computerSelection == "scissors") :
             playerScore += 1;
-            return "You win! Rock beats scissors!";
+            return ["You win! Rock beats scissors!", 'green'];
             break;
         case (playerSelection == "paper") && (computerSelection == "scissors") :
             computerScore += 1;
-            return "You lose! Scissors beats Paper!";
+            return ["You lose! Scissors beats Paper!", 'red'];
             break;
         case (playerSelection == "paper") && (computerSelection == "rock") :
             playerScore += 1;
-            return "You win! Paper beats Rock!";
+            return ["You win! Paper beats Rock!", 'green'];
             break;
         case (playerSelection == "scissors") && (computerSelection == "rock") :
             computerScore += 1;
-            return "You lose! Rock beats Scissors!";
+            return ["You lose! Rock beats Scissors!", 'red'];
             break;
         case (playerSelection == "scissors") && (computerSelection == "paper") :
             playerScore += 1;
-            return "You win! Scissors beats Paper!";
+            return ["You win! Scissors beats Paper!", 'green'];
             break;
         default:
             playerScore += 1;
             computerScore += 1;
-            return `Draw! Both do ${playerSelection}`;
+            return [`Draw! Both do ${playerSelection}`, 'grey'];
             break;            
     }
 }
 
 // This function plays on round
-function playRound() {
-    return;
+function playRound(playerMove, computerMove=computerPlay(moves)) {
+    const statusBar = document.querySelector('.status');
+    let result = getResult(playerMove, computerMove);
+    if (result[1] == 'green') {
+        statusBar.textContent = result[0];
+        statusBar.style.color = result[1];
+    } else {
+        statusBar.textContent = result[0];
+        statusBar.style.color = result[1];
+    }
+    document.querySelector('.player-score').textContent = playerScore;
+    document.querySelector('.comp-score').textContent = computerScore;
 }
 
 // This function plays the game
 function game() {
+    while (gameOn == true){
+        if (playerScore == 5 && computerScore !== 5) {
+            document.querySelector('.status').textContent = `'You have won by a score of ${playerScore} - ${computerScore}!`;
+            gameOn = false;
+        } else if (computerScore == 5 && playerScore !== 5){
+            document.querySelector('.status').textContent = `The computer has won by a score of ${computerScore} - ${playerScore}!`;
+            gameOn = false
+        } else {
+            moves.forEach(move => {
+                document.querySelector(`.${move}`).addEventListener('click', (e) => {
+                    playRound(move);
+                });
+            });
+        }
+    }
+    
     // This event listener is very clever, because it does not initalize unuseful variables
-    moves.forEach(move => {
-        document.querySelector(`.${move}`).addEventListener('click', (e) => {
-            console.log(e.target);
-        });
-    });
+    
 }
 
 game();
